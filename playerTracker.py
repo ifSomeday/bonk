b9c691ef07ee6344df5860843ad8420731687e5b
Will Rice <willrice21@gmail.com>
2020-01-07 23:21:37 -0700

    Update playerTracker.py
    Add help

--- playerTracker.py
+++ playerTracker.py
@@ -6,15 +6,18 @@ import pickle
 import asyncio
 import os
 
+## Used to track the worst players in MM
 class PlayerTracker(commands.Cog):
 
+    ## Init
     def __init__(self, bot):
         self.bot = bot
         self.fileLock = asyncio.Lock()
         self.filePath = "{0}/trackerDB.pickle".format(os.getcwd())
         self.loadDatabase()
 
-    @commands.command()
+    ## Adds name and comments to the tracker
+    @commands.command(help="Add a player to the MM Tracker")
     async def add(self, ctx, name : str, *, description : str):
         print("adding {0} as {1}".format(name, description))
         self.database.setdefault(name.lower(), {"name" : name, "description" : []})
@@ -22,11 +25,11 @@ class PlayerTracker(commands.Cog):
         await self.saveDatabase()
         await ctx.message.add_reaction('✅')
         
-
-    @commands.command()
+    ## searches the tracker for player matches based on query
+    ## this is a rudimentary case insensitive substring search 
+    @commands.command(help="Search the MM Tracker for players")
     async def search(self, ctx, *, name : str):
         nameL = name.lower()
-        print(self.database)
         out = []
         async with self.fileLock:
             for k in self.database:
@@ -35,27 +38,29 @@ class PlayerTracker(commands.Cog):
         await ctx.send("\n".join(out))
             
 
-
-    @commands.command()
+    ## display a player and their comments
+    @commands.command(help="Display information about a specific player in the MM Tracker")
     async def display(self, ctx, *, name : str):
         async with self.fileLock:
             if name.lower() in self.database:
-                desc = " **-** {0}".format("\n **-** ".join(self.database[name.lower()]["description"]))
+                user = self.database[name.lower()]
+                desc = " **-** {0}".format("\n **-** ".join(user["description"]))
                 embed = discord.Embed()
                 embed.color = discord.Colour.purple()
-                embed.title = self.database[name.lower()]["name"]
+                embed.title = user["name"]
                 embed.description = desc
                 await ctx.send(embed=embed)
             else:
                 await ctx.send("`{0}` not found".format(name))
 
-
+    ## Load the tracker database (TODO: convert to psql)
     def loadDatabase(self):
         self.database = {}
         if(os.path.isfile(self.filePath)):
             with open(self.filePath, "rb") as f:
                 self.database = pickle.load(f)
 
+    ## Save the tracker database (TODO: convert to psql)
     async def saveDatabase(self):
         async with self.fileLock:
             with open(self.filePath, "wb") as f:
