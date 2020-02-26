47f73186a1f1c1a89d3c77556b9814266f410467
Will Rice <willrice21@gmail.com>
2020-02-26 02:02:08 -0700

    Update stream.py
    switch to custom async twitch library

--- stream.py
+++ stream.py
@@ -7,12 +7,14 @@ import twitch
 import os
 import asyncio
 import pickle
+import aiohttp
+import traceback
+
 
 class Stream(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.helix = twitch.Helix(keys.TWITCH_CLIENT_ID)
         self.filePath = "{0}/streams.pickle".format(os.getcwd())
         self.alertPath = "{0}/alerts.pickle".format(os.getcwd())
         self.fileLock = asyncio.Lock()
@@ -23,40 +25,97 @@ class Stream(commands.Cog):
 
         self.alertChannels = []
         self.loadAlerts()
-        
-        self.checkStream.start()
 
+        self.twitchAccessTokenUrl = "https://id.twitch.tv/oauth2/token"
+        self.twitchUserUrl = "https://api.twitch.tv/helix/users"
+        self.twitchRevokeTokenUrl = "https://id.twitch.tv/oauth2/revoke"
+        self.twitchStreamUrl = "https://api.twitch.tv/helix/streams"
+
+        self.checkStream.start()
 
     @tasks.loop(seconds=60.0)
     async def checkStream(self):
         try:
-            async with self.fileLock:
-                for user in self.database:
-                    channel = self.helix.user(user)
-                    if(channel is None):
-                        return
-                    if(not self.database[user]["online"] == channel.is_live):
-                        if(channel.is_live):
-                            embed = await self.buildEmbed(channel)
+            accessToken = await self.__getAccessToken()
+           
+            for user in self.database:
+                 async with self.fileLock:
+                    channel = await self.__getTwitchChannel(user, accessToken)
+                    if(channel is None or len(channel) == 0):
+                        continue
+                    channel = channel[0]
+                    stream = await self.__getTwitchStream(channel["id"], accessToken)
+                    if(stream is None or len(stream) == 0):
+                        continue
+                    stream = stream[0]
+                    isLive = stream["type"] == "live"
+                    if(not self.database[user]["online"] == isLive or True):
+                        if(isLive):
+                            embed = await self.buildEmbed(channel, stream)
                             messageIds = await self.doAlert(embed)
                             self.database[user]["messageIds"] = messageIds
-                    elif(channel.is_live):
+                    elif(isLive):
                         await self.doUpdate(channel, self.database[user]["messageIds"])
-                    self.database[user]["online"] = channel.is_live
+                    self.database[user]["online"] = isLive
             await self.saveDatabase()
         except Exception as e:
             print("Fucky Wucky:\n{0}".format(e))
+            traceback.print_exc()
+        finally:
+            await self.__revokeAccessToken(accessToken)
+
+    
 
 
-    async def buildEmbed(self, channel):
-        stream = channel.stream
+    async def __getAccessToken(self):
+        async with aiohttp.ClientSession() as session:
+            params = { 'client_id' : keys.TWITCH_BONK2_ID,
+                'client_secret' : keys.TWITCH_BONK2_SECRET,
+                'grant_type' : 'client_credentials' }
+            async with session.post(self.twitchAccessTokenUrl, params = params) as r:
+                if r.status == 200:
+                    js = await r.json()
+                    return(js["access_token"])
+
+
+    async def __getTwitchStream(self, userId, access_token):
+        async with aiohttp.ClientSession() as session:
+            params = { 'user_id' : userId,
+                'client_id' : keys.TWITCH_BONK2_ID }
+            header = { 'Authorization' : "Bearer {token}".format(token = access_token) }
+            async with session.get(self.twitchStreamUrl, params = params, headers=header) as r:
+                if r.status == 200:
+                    js = await r.json()
+                    return(js['data'])
+
+
+    async def __getTwitchChannel(self, user, access_token):
+        async with aiohttp.ClientSession() as session:
+            params = { 'login' : user,
+                'client_id' : keys.TWITCH_BONK2_ID }
+            header = { 'Authorization' : "Bearer {token}".format(token = access_token) }
+            async with session.get(self.twitchUserUrl, params = params, headers=header) as r:
+                if r.status == 200:
+                    js = await r.json()
+                    return(js['data'])
+                    
+
+    async def __revokeAccessToken(self, access_token):
+        async with aiohttp.ClientSession() as session:
+            params = { 'token' : access_token,
+                'client_id' : keys.TWITCH_BONK2_ID }
+            async with session.post(self.twitchRevokeTokenUrl, params = params) as r:
+                if r.status == 200:
+                    print("token revoked")
+
+    async def buildEmbed(self, channel, stream):
         embed = discord.Embed()
-        embed.title = stream.title
-        embed.set_thumbnail(url=stream.thumbnail_url.format(height=720, width=1280))
-        embed.set_author(name=channel.display_name, url="https://www.twitch.tv/{0}".format(stream.user), icon_url=channel.profile_image_url)
-        embed.url = "https://www.twitch.tv/{0}".format(stream.user)
+        embed.title = stream["title"]
+        embed.set_thumbnail(url=stream['thumbnail_url'].format(height=720, width=1280))
+        embed.set_author(name=channel['display_name'], url="https://www.twitch.tv/{0}".format(stream['user_name']), icon_url=channel['profile_image_url'])
+        embed.url = "https://www.twitch.tv/{0}".format(stream['user_name'])
         embed.color = discord.Colour.purple()
-        embed.set_footer(text="{0} Viewers".format(stream.viewer_count))
+        embed.set_footer(text="{0} Viewers".format(stream['viewer_count']))
         return(embed)
 
 
@@ -87,7 +146,10 @@ class Stream(commands.Cog):
     async def addStream(self, ctx, *, streamId : str):
         streamId = streamId.lower()
         if(not streamId in self.database):
-            if(not self.helix.user(streamId) == None):
+            accessToken = await self.__getAccessToken()
+            channel = await self.__getTwitchChannel(streamId, accessToken)
+            await self.__revokeAccessToken(accessToken)
+            if(not (channel is None or len(channel) == 0)):
                 self.database.setdefault(streamId, {"online" : False, "messageIds" : []})
                 await self.saveDatabase()
                 await ctx.message.add_reaction('✅')
