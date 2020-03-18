8df75e383e9beaff00a11dd96aad6df8c5d374ea
Will Rice <willrice21@gmail.com>
2020-03-18 13:10:09 -0600

    Update misc.py
    add save

--- misc.py
+++ misc.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands, tasks
 
+import io
+import aiohttp
+
 def is_me(**perms):
     async def predicate(ctx):
         return(ctx.message.author.id == 133811493778096128)
@@ -13,6 +16,7 @@ class Misc(commands.Cog):
 
         self.tesxt = "sup loser. Beep. Boop. Assuming you actually have them, you can invite friends to this server.  Just click the server name in the top left and select \"Invite People\". Beep beep, bitch."
 
+        self.hamHamUrl = "https://cdn.discordapp.com/attachments/427380139186323459/568038125927137280/hamham.png"
 
     @commands.command()
     @is_me()
@@ -20,9 +24,26 @@ class Misc(commands.Cog):
         print(user.name)
         await user.send(self.tesxt)
 
-    @commands.command(command_prefix="!")
+    @commands.command(help="hamham")
     async def save(self, ctx):
-        print("hi")
+        #await self.sendHamHam(ctx)
+        pass
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if(any(x in ctx.clean_content for x in [".save", "!save"])):
+            await self.sendHamHam(ctx, ctx.channel)
+
+
+    async def sendHamHam(self, ctx, destination):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.hamHamUrl) as r:
+                if r.status != 200:
+                    await ctx.send("Error retrieving hamham :(")
+                else:
+                    data = io.BytesIO(await r.read())
+                    await destination.send(file=discord.File(data, "hamham.png"))
+        
 
 def setup(bot):
     bot.add_cog(Misc(bot))
